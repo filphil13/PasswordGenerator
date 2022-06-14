65e64731f88a37359fef17398b0170a42fd1df25
Filipe Madureira <36753415+filphil13@users.noreply.github.com>
2022-06-14 18:35:52 +0000

    Initial commit

---
+++ PasswordGenerator.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"math/rand"
+)
+
+func main() {
+
+	//assigning variables
+	string passWordGenerated;
+	
+	letters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	numbers := "1234567890";
+	symbols := "~`! @#$%^&*()_-+={[}]|\\:;\"'<,>.?/" ;
+
+	if len(arg)  > 2
+	//setting what contents is unneeded in password and storing it in a string
+	//setting how many chars are needed for password
+
+	
+	
+}
