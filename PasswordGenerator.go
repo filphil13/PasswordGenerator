aed673d0b520d97c3f2017222b78274e5e49dd20
filphil12 <filphil13@gmail.com>
2022-06-15 17:03:29 +0000

    fixed comments

--- PasswordGenerator.go
+++ PasswordGenerator.go
@@ -8,37 +8,34 @@ import (
 	"strings"
 )
 
-//assigning variables
 //default attributes of passwordGenrator Request
-
 var CHARMAX int = 64
 var length int = 16
 var amount int = 1
 
-//List of Letters,Numbers, and Symbols
-var rootSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`! @#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
-
-//Takes Args and adjusts correct rootsymbol, and length
-
 func main() {
 
+	//List of Letters,Numbers, and Symbols
 	var rootSymbols string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`! @#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
-	//Handling Arguments
-	//setting what contents is unneeded in password and storing it in a string
-	//setting how many chars are needed for password
 
-	//Password creation
+	//handles arguments
 	rootSymbols = HandlingArgs(rootSymbols)
+
+	//turns rootSymbols into rune to be able to read all symbols
 	rootSymbolsRune := []rune(rootSymbols)
 
-	fmt.Println("Hello")
 	for i := 0; i < amount; i++ {
+
+		//Password creation
 		password := createPassword(rootSymbolsRune)
+
 		//output
 		fmt.Println(password)
 	}
 }
 
+//Handling Arguments
+//Takes Args and adjusts correct rootsymbol, and length
 func HandlingArgs(rootSymbols string) string {
 	newRootSymbols := rootSymbols
 
@@ -51,23 +48,29 @@ func HandlingArgs(rootSymbols string) string {
 			if newLength, err := strconv.Atoi(arg); err == nil {
 				length = newLength
 
+				//Disables all letters
 			} else if arg == "-al" {
 				newRootSymbols = strings.ReplaceAll(newRootSymbols, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "")
 
+				//disables all lower letters
 			} else if arg == "-ll" {
 				newRootSymbols = strings.ReplaceAll(newRootSymbols, "abcdefghijklmnopqrstuvwxyz", "")
 
+				//disables all capital letters
 			} else if arg == "-cl" {
 				newRootSymbols = strings.ReplaceAll(newRootSymbols, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "")
 
+				//disables all numbers
 			} else if arg == "-n" {
 				newRootSymbols = strings.ReplaceAll(newRootSymbols, "1234567890", "")
 
+				//disbales all symbols
 			} else if arg == "-s" {
 				newRootSymbols = strings.ReplaceAll(newRootSymbols, "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/", "")
 			}
 		}
 	}
+	//returns new string with argument modifications
 	return newRootSymbols
 
 }
@@ -77,6 +80,8 @@ func createPassword(rootSymbolsRune []rune) string {
 
 	for i := 0; i < length; i++ {
 
+		//chooses random index of rune array and and adds it to a variable
+		//keeps repeating this until password length is satisfied
 		randomIndex := rand.Intn(len(rootSymbolsRune))
 		password = password + string(rootSymbolsRune[randomIndex])
 	}
